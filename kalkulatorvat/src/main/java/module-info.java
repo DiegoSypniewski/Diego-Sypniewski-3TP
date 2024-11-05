296d5f14ab939699fff478b42b344c7482bee77e
DiegoSypniewski <diego_sypniewski@outlook.com>
2024-11-05 11:04:05 +0100

    Delete kalkulatorvat directory

--- kalkulatorvat/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.kalkulatorvat {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.kalkulatorvat to javafx.fxml;
-    exports com.example.kalkulatorvat;
-}
